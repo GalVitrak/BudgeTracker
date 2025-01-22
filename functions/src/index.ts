5a2a6022ba1018577302bfa2e835549cc3fff158
GalVitrak <galvit25@gmail.com>
2025-01-22 02:33:49 +0200

    removed functions for getting data from DB

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,7 @@
 import * as admin from "firebase-admin";
 import register from "./register";
 import login from "./login";
-import getSpendings from "./getSpending";
 import addSpending from "./addSpending";
-import getCategories from "./getCategories";
 import addCategory from "./addCategory";
 import addSubCategory from "./addSubCategory";
 
@@ -13,8 +11,6 @@ export const db = admin.firestore();
 
 exports.register = register;
 exports.login = login;
-exports.getSpendings = getSpendings;
 exports.addSpending = addSpending;
-exports.getCategories = getCategories;
 exports.addCategory = addCategory;
 exports.addSubCategory = addSubCategory;
--- src/Services/SpendingsService.ts
+++ src/Services/SpendingsService.ts
@@ -3,54 +3,53 @@ import { functions } from "../../firebase-config";
 import { authStore } from "../Redux/AuthState";
 import notifyService from "./NotifyService";
 import SpendingModel from "../Models/SpendingModel";
-import dayjs from "dayjs";
 import CategoryModel from "../Models/CategoryModel";
 
 class SpendingsService {
-  public async getSpendings(): Promise<
-    SpendingModel[]
-  > {
-    const getSpendings = httpsCallable(
-      functions,
-      "getSpendings"
-    );
+  // public async getSpendings(): Promise<
+  //   SpendingModel[]
+  // > {
+  //   const getSpendings = httpsCallable(
+  //     functions,
+  //     "getSpendings"
+  //   );
 
-    const uid = authStore.getState().user?.uid;
+  //   const uid = authStore.getState().user?.uid;
 
-    let spendings: SpendingModel[] = [];
+  //   let spendings: SpendingModel[] = [];
 
-    if (!uid) {
-      notifyService.error(
-        "User is not logged in"
-      );
-      return spendings;
-    }
+  //   if (!uid) {
+  //     notifyService.error(
+  //       "User is not logged in"
+  //     );
+  //     return spendings;
+  //   }
 
-    await getSpendings({ uid })
-      .then((result) => {
-        const data = result.data as [];
-        data.forEach((spending: any) => {
-          spendings.push(
-            new SpendingModel(
-              spending.uid,
-              spending.category,
-              spending.subCategory,
-              dayjs
-                .unix(spending.date._seconds)
-                .format("DD.MM.YYYY"),
-              spending.sum,
-              spending.note,
-              spending.id
-            )
-          );
-        });
-      })
-      .catch((error) => {
-        notifyService.error(error);
-        throw error;
-      });
-    return spendings;
-  }
+  //   await getSpendings({ uid })
+  //     .then((result) => {
+  //       const data = result.data as [];
+  //       data.forEach((spending: any) => {
+  //         spendings.push(
+  //           new SpendingModel(
+  //             spending.uid,
+  //             spending.category,
+  //             spending.subCategory,
+  //             dayjs
+  //               .unix(spending.date._seconds)
+  //               .format("DD.MM.YYYY"),
+  //             spending.sum,
+  //             spending.note,
+  //             spending.id
+  //           )
+  //         );
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       notifyService.error(error);
+  //       throw error;
+  //     });
+  //   return spendings;
+  // }
 
   public async addSpending(
     spending: SpendingModel
